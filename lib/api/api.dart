bf73b21baa4849e2eef7d327589a28e8eabc55af
Antonino Praxedes <antonino.praxedes@gmail.com>
2022-08-09 08:20:08 -0400

    feat: access post api ready

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,12 +1,12 @@
 import 'package:dio/dio.dart';
 
 class Api {
-  Future createTtr(String tecnicoId, String tombo, String destino,
+  Future createTtr(int tecnicoId, String tombo, String destino,
       String responsavel, String observacao) async {
     try {
       var response =
           await Dio().post('http://10.50.16.93:3000/ttrs/create', data: {
-        'tecnicoId': '$tecnicoId',
+        'n2_tecnico_id': tecnicoId,
         'tombo': '$tombo',
         'destino': '$destino',
         'responsavel': '$responsavel',
